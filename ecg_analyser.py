a6bc0b479cc0c58d1a52bd7c21ec3bbaedb8c1bc
YDego <yehonatand123@gmail.com>
2023-04-19 14:08:20 +0300

    plots all the graphs and specific signal from it (v6), now written better, units and labels needs to be fixed

--- ecg_analyser.py
+++ ecg_analyser.py
@@ -1,18 +1,34 @@
 from IPython.display import display
-
-#import matplotlib.pyplot as plt
-#import numpy as np
-#import os
-#import shutil
-#import posixpath
 import wfdb
+import os
 
-record = wfdb.rdrecord(r'ecg_dataset\lobachevsky-university-electrocardiography-database-1.0.1\data\57')
-#record = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
 
-signal = record.p_signal[:, 5]
+# Chose data file
+data_dir = 'ecg_dataset/lobachevsky-university-electrocardiography-database-1.0.1/data'
+file_count = len(os.listdir(data_dir))
+data_file = str(input(f"There are {file_count} data files, choose one: "))
 
-# Plot signal
-wfdb.plot_wfdb(record=record, title='ECG')
-wfdb.plot_items(signal=signal, fs=record.fs, title='ECG Lead V6')
-display(record.__dict__)
+# Define the list of leads available
+leads = ['i', 'ii', 'iii', 'avr', 'avl', 'avf', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'All']
+
+# Get user input for which lead to plot
+lead = input(f"Choose a lead to plot ({', '.join(leads)}): ")
+
+# Check if the user input is valid
+if lead not in leads:
+    print("Invalid lead selection!")
+elif lead == 'All' or lead == 'all':
+    # Read the record and plot the selected lead
+    record_path = fr'ecg_dataset\lobachevsky-university-electrocardiography-database-1.0.1\data\{data_file}'
+    record = wfdb.rdrecord(record_path)
+    # record = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
+    wfdb.plot_wfdb(record=record, title='ECG')
+else:
+    # Read the record and plot the selected lead
+    record_path = fr'ecg_dataset\lobachevsky-university-electrocardiography-database-1.0.1\data\{data_file}'
+    record = wfdb.rdrecord(record_path)
+    # record = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
+    signal = record.__dict__['p_signal'][:, leads.index(lead)]
+    wfdb.plot_items(signal=signal, fs=record.fs, title=f'ECG Lead {lead}', time_units='seconds', sig_name='mV')
+
+    display(record.__dict__)
