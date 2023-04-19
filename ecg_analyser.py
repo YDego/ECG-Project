b3915cf32c8b057dd7e264a35a18e6b111b58bd4
YDego <yehonatand123@gmail.com>
2023-04-19 13:21:39 +0300

    plots all the graphs and specific signal from it (v6)

--- ecg_analyser.py
+++ ecg_analyser.py
@@ -5,13 +5,14 @@ from IPython.display import display
 #import os
 #import shutil
 #import posixpath
-
 import wfdb
 
-SAMPLE_RATE = 500 #[Hz]
-
-record = wfdb.rdrecord(r'ecg_dataset\lobachevsky-university-electrocardiography-database-1.0.1\data\1')
+record = wfdb.rdrecord(r'ecg_dataset\lobachevsky-university-electrocardiography-database-1.0.1\data\57')
 #record = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
 
+signal = record.p_signal[:, 5]
+
+# Plot signal
 wfdb.plot_wfdb(record=record, title='ECG')
+wfdb.plot_items(signal=signal, fs=record.fs, title='ECG Lead V6')
 display(record.__dict__)
