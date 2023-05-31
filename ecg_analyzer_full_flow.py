6cb143241b2912c92667c04a9208baffe0226f55
YDego <yehonatand123@gmail.com>
2023-05-31 23:59:22 +0300

    while canceled

--- ecg_analyzer_full_flow.py
+++ ecg_analyzer_full_flow.py
@@ -1,25 +1,23 @@
 import processing_functions
-import qrs_detection
 import lead_extractor
 import plot_manager
+# import qrs_detection
 
 
-while input("Choose new datafile? [Y/n]: ") != 'n':
-    # Call the function with leads and file_count as inputs
-    ecg_original = lead_extractor.choose_lead_from_dataset()
+# Call the function with leads and file_count as inputs
+ecg_original = lead_extractor.choose_lead_from_dataset()
 
-    # Plot signal
-    ecg_original["fft"], ecg_original["frequency_bins"] = processing_functions.compute_fft(ecg_original["signal"], ecg_original["fs"])
-    plot_manager.plot_single_signal(ecg_original)
+# Plot signal
+ecg_original["fft"], ecg_original["frequency_bins"] = processing_functions.compute_fft(ecg_original["signal"], ecg_original["fs"])
+plot_manager.plot_single_signal(ecg_original)
 
-    # ECG processing
-    ecg_processed = processing_functions.ecg_pre_processing(ecg_original)
+# ECG processing
+ecg_processed = processing_functions.ecg_pre_processing(ecg_original)
 
-    # ecg_wavelet = ecg_original.copy()
+# ecg_wavelet = ecg_original.copy()
 
-    # ecg_wavelet["signal"] = qrs_detection.wavelet_filter(ecg_original["signal"])
+# ecg_wavelet["signal"] = qrs_detection.wavelet_filter(ecg_original["signal"])
 
-    ecg_processed["fft"], ecg_processed["frequency_bins"] = processing_functions.compute_fft(ecg_processed["signal"], ecg_processed["fs"])
+ecg_processed["fft"], ecg_processed["frequency_bins"] = processing_functions.compute_fft(ecg_processed["signal"], ecg_processed["fs"])
 
-    plot_manager.plot_original_vs_processed(ecg_original, ecg_processed)
-
+plot_manager.plot_original_vs_processed(ecg_original, ecg_processed)
