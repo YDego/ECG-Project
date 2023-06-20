9063fdf97aa98257dd9f1fb41367b9ce0de71253
shahar <shahar980000s@gmail.com>
2023-06-20 20:32:13 +0300

    ...

--- qrs_detection.py
+++ qrs_detection.py
@@ -7,26 +7,22 @@ import numpy as np
 def detection_qrs(ecg_original_copy):
     original_signal = ecg_original_copy["signal"]
     fs = ecg_original_copy["fs"]
-    re_check_samples = 100
-    impossible_margin = 150
-    new_signal = processing_functions.band_pass_filter(8, 50, original_signal, fs)
+    re_check_samples = round(0.2*fs)
+    new_signal = processing_functions.band_pass_filter(8, 49, original_signal, fs)
     signal = abs(new_signal)**3
     ecg_original_copy["signal"] = signal
-    ecg_original_copy["fft"] , ecg_original_copy["frequency_bins"] = processing_functions.compute_fft(signal, fs)
+    ecg_original_copy["fft"], ecg_original_copy["frequency_bins"] = processing_functions.compute_fft(signal, fs)
     threshold = np.mean(signal)
 
-    open_dots, closed_dots, all_dots = detection_qrs_aux_new(signal, threshold, 0.4, 0, False)
-    single_open_dots, single_closed_dots = check_for_singles_dots(open_dots, closed_dots, all_dots)
-
+    open_dots, closed_dots, all_dots = detection_qrs_aux_new(signal, threshold, 0.4, 0, False, fs)
+    single_open_dots, single_closed_dots = check_for_singles_dots(open_dots, closed_dots, all_dots, fs)
 
     for dot in single_open_dots:
         if dot > len(signal) - re_check_samples:
             open_dots = np.delete(open_dots , np.where(open_dots == dot))
             all_dots = np.delete(all_dots, np.where(all_dots == dot))
             continue
-        #elif np.any(open_dots[abs(open_dots - dot) < impossible_margin]):
-            #continue
-        _, extra_close_dot, _ = detection_qrs_aux_new(signal[dot:dot+re_check_samples], threshold/2, 0.6, 1, True)
+        _, extra_close_dot, _ = detection_qrs_aux_new(signal[dot:dot+re_check_samples], threshold/2, 0.6, 1, True, fs)
         closed_dots = np.concatenate((extra_close_dot + dot, closed_dots))
 
     for dot in single_closed_dots:
@@ -34,15 +30,13 @@ def detection_qrs(ecg_original_copy):
             closed_dots = np.delete(closed_dots, np.where(closed_dots == dot))
             all_dots = np.delete(all_dots, np.where(all_dots == dot))
             continue
-        #elif np.any(closed_dots[abs(closed_dots - dot) < impossible_margin]):
-            #continue
-        extra_open_dot, _, _ = detection_qrs_aux_new(signal[dot - re_check_samples:dot], threshold/2, 0.6, 0, True)
+        extra_open_dot, _, _ = detection_qrs_aux_new(signal[dot - re_check_samples:dot], threshold/2, 0.6, 0, True, fs)
         open_dots = np.concatenate((dot - extra_open_dot, open_dots))
 
     closed_dots = sorted(closed_dots)
     open_dots = sorted(open_dots)
-    R_peaks_potential = wfdb.processing.find_local_peaks(original_signal, radius=25)
-    r_peaks = find_r_peak(R_peaks_potential, open_dots, closed_dots , original_signal)
+    r_peaks_potential = wfdb.processing.find_local_peaks(original_signal, radius=round(0.05*fs))## todo
+    r_peaks = find_r_peak(r_peaks_potential, open_dots, closed_dots, original_signal, fs)
     all_dots = sorted(np.concatenate((open_dots, r_peaks, closed_dots)))
     ecg_original_copy["our_ann"] = all_dots
     ecg_original_copy["our_ann_markers"] = np.zeros(len(all_dots), dtype=str)
@@ -56,8 +50,8 @@ def detection_qrs(ecg_original_copy):
 
     return ecg_original_copy
 
-
-def detection_qrs_aux(signal, threshold, margin_error, start_location, one_point):
+"""
+def detection_qrs_aux(signal, threshold, margin_error, start_location, one_point, fs):
     location = start_location
     location_for_open = 0
     location_for_closed = 0
@@ -65,8 +59,8 @@ def detection_qrs_aux(signal, threshold, margin_error, start_location, one_point
     #print(signal)
     times_repair_open = 0
     times_repair_closed = 0
-    steps_to_check = 15
-    impossible_margin = 100
+    steps_to_check = round(0.03*fs)
+    impossible_margin = round(0.2*fs)
     fix_error = 50
     open_dots = np.zeros(MAX_NUM_PULSE, dtype=int)
     closed_dots = np.zeros(MAX_NUM_PULSE, dtype=int)
@@ -115,28 +109,23 @@ def detection_qrs_aux(signal, threshold, margin_error, start_location, one_point
     all_dots = np.concatenate((open_dots, closed_dots))
     all_dots = sorted(all_dots)
     return open_dots, closed_dots, all_dots
+"""
 
 
-
-
-
-
-
-
-def detection_qrs_aux_new(signal, threshold, margin_error, start_from, one_point):
+def detection_qrs_aux_new(signal, threshold, margin_error, start_from, one_point, fs):
     location = start_from
     location_for_open = 0
     location_for_closed = 0
-    MAX_NUM_PULSE = 5000
+    max_num_dots = (fs * 100)
     # print(signal)
     times_repair_open = 0
     times_repair_closed = 0
-    steps_to_check = 15
-    impossible_margin = 100
-    fix_error = 50
-    max_of_repair = 10
-    open_dots = np.zeros(MAX_NUM_PULSE, dtype=int)
-    closed_dots = np.zeros(MAX_NUM_PULSE, dtype=int)
+    steps_to_check = round(0.03*fs) ## 15
+    impossible_margin = round(0.2*fs) ## 100
+    fix_error = round(0.1*fs) ## 50
+    max_of_repair = round(0.02*fs) ## 10
+    open_dots = np.zeros(max_num_dots, dtype=int)
+    closed_dots = np.zeros(max_num_dots, dtype=int)
     repair_dots = []
 
     if location == 0:
@@ -232,7 +221,8 @@ def check_radius_open_dot(signal, index, threshold, distance, margin_error):
         flag_backward = False
     return flag_backward and flag_forward
 
-def check_radius_closed_dot(signal, index ,threshold, distance, margin_error):
+
+def check_radius_closed_dot(signal, index, threshold, distance, margin_error):
     flag_forward = True
 
     if threshold < np.mean(signal[index:index+distance]):#new
@@ -266,9 +256,9 @@ def r_peaks_annotations(ecg_original, chosen_ann):
 ### changed at 16.6.23 - 22:45
 def comprasion_r_peaks(ecg_dict):
     r_peaks_real_annotations = r_peaks_annotations(ecg_dict, 'real')
-    #print(r_peaks_real_annotations)
+    print(r_peaks_real_annotations)
     r_peaks_our_annotations = r_peaks_annotations(ecg_dict, 'our')
-    #print(r_peaks_our_annotations)
+    print(r_peaks_our_annotations)
     len_iter = min(len(r_peaks_real_annotations), len(r_peaks_our_annotations))
     location_to_start = 0
     min_distance = math.inf
@@ -285,18 +275,16 @@ def comprasion_r_peaks(ecg_dict):
     for index in range(len_iter):
         distance_from_real[index] = abs(r_peaks_real_annotations[index] - r_peaks_our_annotations[index])
         number_of_dots = number_of_dots + 1
-        if distance_from_real[index] <= 3:## 6 ms
+        if distance_from_real[index] <= 10:## 6 ms
             success = success + 1
     ecg_dict["r_peak_success"] = [success, number_of_dots]
     return ecg_dict
 
 
-def check_for_singles_dots(open_dots
-                    ,closed_dots,
-                           all_dots):
+def check_for_singles_dots(open_dots, closed_dots, all_dots, fs):
     ## check that closed dot comes after open and there is not 2 open dot in a row
 
-    impossible_margin = 150
+    impossible_margin = round(0.3*fs)
     list_of_single_open_dots = []
     list_of_single_closed_dots = []
     index_open, index_close = 0, 0
@@ -322,8 +310,10 @@ def check_for_singles_dots(open_dots
 
     return list_of_single_open_dots, list_of_single_closed_dots
 
-
-def find_r_peak(r_peak_potential , q_peak , s_peak , original_signal):
+#todo
+def find_r_peak(r_peak_potential, q_peak, s_peak, original_signal, fs):
+    r_peak_potential = wfdb.processing.find_local_peaks(original_signal, radius=round(0.05 * fs))
+    r_peak_potential_minimum = wfdb.processing.find_local_peaks((-1)*original_signal, radius=round(0.05 * fs))
     r_peak = np.zeros(len(q_peak), dtype=int)
     for index, value in enumerate(q_peak):
         potential_r_peak_one_interval = {}
@@ -334,5 +324,14 @@ def find_r_peak(r_peak_potential , q_peak , s_peak , original_signal):
                 continue
         if len(potential_r_peak_one_interval) != 0:
             r_peak[index] = max(potential_r_peak_one_interval, key=potential_r_peak_one_interval.get)
-    return r_peak
 
+        else:
+            for value_r_minimum in r_peak_potential_minimum:
+                if q_peak[index] < value_r_minimum < s_peak[index]:
+                    potential_r_peak_one_interval[value_r_minimum] = original_signal[value_r_minimum]
+                else:
+                    continue
+            if len(potential_r_peak_one_interval) != 0:
+                r_peak[index] = min(potential_r_peak_one_interval, key=potential_r_peak_one_interval.get)
+
+    return r_peak
