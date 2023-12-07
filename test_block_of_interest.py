bcd4592cc655979c055494970196734a693ea060
YDego <yehonatand123@gmail.com>
2023-12-07 21:41:51 +0200

    in progress

--- test_block_of_interest.py
+++ test_block_of_interest.py
@@ -9,8 +9,10 @@ import processing_functions as pf
 import scipy
 import time
 
-success = 0
+success_per_seg = 0
+success_per_peak = 0
 number_of_t_dots = 0
+number_of_segments = 0
 count = 0
 w1_size = 0.070
 dataset = 'ludb'
@@ -18,7 +20,7 @@ data = []
 time_per_record = []
 signal_len_in_time = 900
 
-for i in range(14, 200, 1):
+for i in range(1, 200, 1):
 
     # if i in [38, 71, 88, 95, 101, 109, 111] and dataset == 'ludb': #
     #   continue
@@ -112,93 +114,16 @@ for i in range(14, 200, 1):
             selected_peak, peak_is_correct = t_wave_detection.calculate_total_score(signal_without_dc, signal_moving_average, t_min, t_max, qf_min_avg, qf_max_avg, norm_idx_minima, norm_idx_maxima, inverted)
             all_peaks_correct = all_peaks_correct and peak_is_correct
             t_peak_location.append(selected_peak)
+            number_of_t_dots += 1
+            success_per_peak += int(peak_is_correct)
 
-        # ratio_factor = 1.5
-        # t_peak = np.zeros(t_peak_maxima.size, dtype=int)
-        # if t_peak_minima.size == t_peak_maxima.size:
-        #     #pm.plot_signal_with_dots2(signal_without_qrs, t_peak_minima, t_peak_maxima, fs, 'original signal', 'low t peaks', 'our t peaks', i, seg, signal_len_in_time)
-        #     waiting_queue = []
-        #     normal_peak = 0
-        #     inverted_peak = 0
-        #     signal_moving_average = t_wave_detection.moving_average(signal_without_dc, 0.2 * fs + 1)
-        #     #pm.plot_2_signals(signal_without_dc, signal_moving_average, fs, label1='signal_without_dc', label2='signal_moving_average')
-        #     for index in range(t_peak.size):
-        #         if t_peak_minima[index] == -1:
-        #             t_peak[index] = t_peak_maxima[index]
-        #             normal_peak += 1
-        #             continue
-        #         elif t_peak_maxima[index] == -1:
-        #             t_peak[index] = t_peak_minima[index]
-        #             inverted_peak += 1
-        #             continue
-        #         elif t_peak_maxima[index] == -1 and t_peak_minima[index] == -1:
-        #             t_peak[index] = 0
-        #             continue
-        #         rr_interval = r_peaks[index + 1] - r_peaks[index]
-        #         #rt_min = int(np.ceil(0.17 * rr_interval))
-        #         rt_max = int(np.ceil(0.5 * rr_interval))
-        #         #x = np.abs(signal_without_dc[t_peak_maxima[index]] - signal_moving_average[t_peak_maxima[index]])
-        #         #y = np.abs(signal_without_dc[t_peak_minima[index]] - signal_moving_average[t_peak_minima[index]])
-        #         #x_norm_sum = np.sum(np.abs(signal_without_dc[t_start_maxima[index]:t_end_maxima[index]] - signal_moving_average[t_start_maxima[index]:t_end_maxima[index]])) / (t_end_maxima[index] - t_start_maxima[index])
-        #         #y_norm_sum = np.sum(np.abs(signal_without_dc[t_start_minima[index]:t_end_minima[index]] - signal_moving_average[t_start_minima[index]:t_end_minima[index]]) / (t_end_minima[index] - t_start_minima[index]))
-        #         if ratio_factor * np.abs(signal_without_dc[t_peak_maxima[index]] - signal_moving_average[t_peak_maxima[index]]) < np.abs(signal_without_dc[t_peak_minima[index]] - signal_moving_average[t_peak_minima[index]]):
-        #             t_peak[index] = t_peak_minima[index]
-        #             inverted_peak += 1
-        #         elif np.abs(signal_without_dc[t_peak_maxima[index]] - signal_moving_average[t_peak_maxima[index]]) > np.abs(signal_without_dc[t_peak_minima[index]] - signal_moving_average[t_peak_minima[index]]) * ratio_factor:
-        #             t_peak[index] = t_peak_maxima[index]
-        #             normal_peak += 1
-        #         else:  # check it TODO
-        #             waiting_queue.append(index)
-        #             #print('not decide')
-        #
-        #     for index in waiting_queue:
-        #         if normal_peak >= inverted_peak and normal_peak != 0:
-        #             t_peak[index] = t_peak_maxima[index]
-        #         elif inverted_peak > normal_peak or signal_without_dc[t_peak_minima[index]] < 0:
-        #             t_peak[index] = t_peak_minima[index]
-        #         else:
-        #             t_peak[index] = t_peak_maxima[index]
-
-        # t_united = np.sort(np.concatenate((t_peak_maxima, t_peak_minima)))
-        # success_record, number_of_t_dots_record = t_wave_detection.comparison_t_peaks(t_real_peaks.copy(),
-        #                                                                               t_united.copy(), fs,
-        #                                                                               r_peaks.size - 1, 0.050)
-        # success += success_record
-        # # number_of_t_dots_record = r_peaks.size - 1
-        # number_of_t_dots += number_of_t_dots_record
-        # if number_of_t_dots_record == 0:
-        #     continue
-        # score = np.round(100 * success_record / number_of_t_dots_record, 5)
-        #
-        # sub_data = [ecg_dict_copy['name'], f'{i}', f'{number_of_t_dots_record}', f'{success_record}', f'{score}']
-        # data.append(sub_data)
-        # # print(i, f'{success_record}/{number_of_t_dots_record}')
-        # if score < 95:
-        #     count += 1
-        # print(i, f'{success_record}/{number_of_t_dots_record}')
-        # end = time.time()
-        # time_per_record.append(end - start)
-        # pm.plot_signal_with_dots2(signal_without_dc, t_peak_maxima, t_peak_minima, fs, 'original signal', 't maxima peaks',
-        #                            't minima peaks', i, seg, signal_len_in_time)
         t_peak_location = np.array(t_peak_location)
 
         if not all_peaks_correct:
             pm.plot_signal_with_dots2(signal_without_dc, t_real_peaks, t_peak_location, fs, 'original signal', 't_real_peaks', 'our t peaks', i, seg, signal_len_in_time)
-if number_of_t_dots != 0:
-    print(round(success * 100 / number_of_t_dots, 5), f'{success}/{number_of_t_dots}', count)
+        success_per_seg += int(all_peaks_correct)
+        number_of_segments += 1
 
-# header = ['record name', 'record number', 'number of beats', 'True Positive', 'Score']
-#
-# with open('qt score manually annotations 30 ms.csv', 'w', encoding='UTF8', newline='') as f:
-#     writer = csv.writer(f)
-#
-#     # write the header
-#     writer.writerow(header)
-#
-#     # write multiple rows
-#     writer.writerows(data)
+print("success per peak = " + str(success_per_peak/number_of_t_dots))
+print("success per segment = " + str(success_per_seg/number_of_segments))
 
-# total_time = sum(time_per_record)
-# print(f'{total_time} sec')
-# #print(f'average time {total_time / i}')
-# print(time_per_record)
